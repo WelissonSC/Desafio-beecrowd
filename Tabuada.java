c49839205782beb96695185756c081f98e66fe2a
WelissonSC <welissonsilvacunha@gmail.com>
2024-08-08 21:52:50 -0300

    outro desafio

---
+++ Tabuada.java
@@ -0,0 +1,15 @@
+package chalengers;
+
+import java.util.Scanner;
+
+public class Tabuada {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(), resul;
+		for(int i = 1 ; i <= 10 ; i++) {
+			resul = i * n;
+			System.out.println(i + " x " + n + " = " + resul);
+			
+		}
+	}
+}
